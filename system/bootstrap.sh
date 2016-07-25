ad57404b16bab2ed98d7c03a34a84e6a809b9720
Neel Smith <dnsmith.neel@gmail.com>
2016-07-25 14:09:15 -0400

    Change bootstrap to install java 8

--- system/bootstrap.sh
+++ system/bootstrap.sh
@@ -29,7 +29,18 @@ apt-get install -y sqlite
 # add jdk later...
 
 
-apt-get install -y openjdk-7-jdk
+#apt-get install -y openjdk-7-jdk
+apt-get -y -q update
+apt-get -y -q upgrade
+apt-get -y -q install software-properties-common htop
+add-apt-repository ppa:webupd8team/java
+apt-get -y -q update
+echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
+echo oracle-java7-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
+apt-get -y -q install oracle-java8-installer
+apt-get -y -q install oracle-java7-installer
+update-java-alternatives -s java-8-oracle
+
 
 # build system and dependency mgt
 apt-get install -y gradle
