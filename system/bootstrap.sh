aab1abf406dd14ef291abbcfd7ee935f98a97ba9
Neel Smith <dnsmith.neel@gmail.com>
2016-07-27 11:09:39 -0400

    update init sequence with goal of single line boot of working CTS

--- system/bootstrap.sh
+++ system/bootstrap.sh
@@ -6,10 +6,6 @@ add-apt-repository ppa:cwchien/gradle
 apt-get update
 
 
-#########################################################
-### Install packages required for HMT editing ###########
-#########################################################
-
 # Clean up any catastrophic reformatting that
 # 'git clone' could introduce on a Windows box:
 apt-get install -y dos2unix
@@ -21,18 +17,17 @@ apt-get install -y dos2unix
 # version control
 apt-get install -y git
 
-# JDK bundle
-#apt-get install -y openjdk-7-jdk
+# JDK bundle.  Need Java 8 for fuseki, but
+# elementary Freya/ubuntu 14.04 only has openjdk-7,
+# so use oracle.
+
 apt-get -y -q upgrade
 apt-get -y -q install software-properties-common htop
 add-apt-repository ppa:webupd8team/java
 apt-get -y -q update
 echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
-echo oracle-java7-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
 apt-get -y -q install oracle-java8-installer
-apt-get -y -q install oracle-java7-installer
 update-java-alternatives -s java-8-oracle
-
 
 
 apt-get -y install groovy
@@ -44,7 +39,8 @@ apt-get install -y apache2
 apt-get install -y libapache2-mod-fastcgi
 apt-get install -y iipimage-server
 
-
+# configuration file with correct JAVA_HOME for our install:
+cp /vagrant/system/etc-default-tomcat7 /etc/default/tomcat7
 
 # Mods to accommodate apache 2.4's default configuration
 # settings:
@@ -83,18 +79,113 @@ chown vagrant:vagrant /home/vagrant/.config/plank/dock1/launchers/*.dockitem
 
 ### SPARLQL END POINT #############################################
 ##
-# 1. Run /vagrant/bin/get-jena.sh script: installs without booting up.
+
+
+JENA_VERSION="apache-jena-3.1.0"
+FUSEKI_VERSION="apache-jena-fuseki-2.4.0"
+
+
+JENA_DIR="/opt/${JENA_VERSION}"
+FUSEKI_DIR="/opt/${FUSEKI_VERSION}"
+
+CURL=`which curl`
+TAR=`which tar`
+RM=`which rm`
+
+# For tdbloader:
+if [ -d $JENA_DIR ]; then
+    echo "apache-jena already installed."
+else
+    echo  "Downloading apache-jena package..."
+    $CURL http://apache.mirrors.pair.com/jena/binaries/${JENA_VERSION}.tar.gz  > /tmp/jena.tgz
+    cd /opt
+    sudo $TAR -zvxf /tmp/jena.tgz
+    $RM /tmp/jena.tgz
+fi
+
+# For fuseki server:
+if [ -d $FUSEKI_DIR ]; then
+    echo "jena-fuseki already installed."
+else
+    echo "Downloading fuseki server..."
+    $CURL http://apache.mirrors.pair.com/jena/binaries/${FUSEKI_VERSION}.tar.gz > /tmp/fuseki.tgz
+    cd /opt
+    sudo $TAR -zxvf /tmp/fuseki.tgz
+    $RM /tmp/fuseki.tgz
+fi
+
+
+
+GIT=`which git`
+echo git installed at $GIT
+
+# This script is repsonsible for cloning or updating all HMT git repositories
+# needed for editing.
+#
+# It's up to individual editors to maintain their own repository
+# for editorial content.
+#
+
+if [ -d "/vagrant/cs2" ]; then
+    echo "Checking for updates to cs2"
+    cd /vagrant/cs2
+    $GIT pull
+else
+    echo "Installing cs2"
+    cd /vagrant
+    echo  Running  $GIT clone
+    https://github.com/cite-architecture/cs2.git
+    $GIT clone https://github.com/cite-architecture/cs2.git
+fi
+
+if [ -d "/vagrant/cite-archive-manager" ]; then
+    echo "Checking for updates to cite-archive-manager"
+    cd /vagrant/cite-archive-manager
+    $GIT pull
+else
+    echo "Installing cite-archive-manager"
+    cd /vagrant
+    echo  Running  $GIT clone     https://github.com/cite-architecture/cite-archive-manager.git
+    $GIT clone https://github.com/cite-architecture/cite-archive-manager.git
+fi
+
+
+if [ -d "/vagrant/easy_cts_archive" ]; then
+    echo "Checking for updates to easy_cts_archive"
+    cd /vagrant/easy_cts_archive
+    $GIT pull
+    cp /vagrant/easy_cts_archive/manager-conf.gradle /vagrant/cite-archive-manager/conf.gradle
+else
+    echo "Installing easy_cts_archive"
+    cd /vagrant
+    echo  Running  $GIT clone
+    https://github.com/cite-architecture/easy_cts_archive.git
+
+    $GIT clone https://github.com/cite-architecture/easy_cts_archive.git
+    cp /vagrant/easy_cts_archive/manager-conf.gradle /vagrant/cite-archive-manager/conf.gradle
+fi
+
+
 # 2. In /vagrant/cite-archive-manager, run gradle ctsttl
 # 3. Run /vagrant/bin/load-ttl.sh script.
 
 
 ### CITE SERVLET  ##################################################
-##
-# 1. Run /vagrant/bin/refresh-repos.sh
-# 2. ....
-#
-# Need to compile cs2, and install in tomcat
+## BUILD DATA:
+cp /vagrant/easy_cts_archive/manager-conf.gradle /vagrant/cite-archive-manager/conf.gradle
 
+cd /vagrant/cite-archive-manager
+
+GRADLE=`which gradle`
+$GRADLE ctsttl
+cp /vagrant/cite-archive-manager/build/ttl/cts.ttl /vagrant/data
+
+/vagrant/bin/load-ttl.sh
+
+# Need to compile cs2, and install in tomcat
+cd /vagrant/cs2/sparqlcts
+$GRADLE war
+cp /vagrant/cs2/sparqlcts/build/lib/sparql\*.war /var/lib/tomcat/webapp/sparqlcts.war
 
 
 ### FURTHER CONFIGURATION  ##########################################
